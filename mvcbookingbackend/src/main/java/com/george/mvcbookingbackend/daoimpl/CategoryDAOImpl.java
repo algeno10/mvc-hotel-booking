730291b807161739d864e31b12873bb01b54be75
George Sikhundla <sikhundla33@gmail.com>
2018-07-02 11:09:43 +0200

    Added categories

--- mvcbookingbackend/src/main/java/com/george/mvcbookingbackend/daoimpl/CategoryDAOImpl.java
+++ mvcbookingbackend/src/main/java/com/george/mvcbookingbackend/daoimpl/CategoryDAOImpl.java
@@ -10,11 +10,46 @@ public class CategoryDAOImpl implements CategoryDAO {
 
 	private static List<Category> categories = new ArrayList<>();
 	
+	static {
+		Category category = new Category();
+		
+		//adding first category
+		category.setId(1);
+		category.setName("Hotel");
+		category.setDescription("This is a description for a hotel");
+		category.setImageURL("CAT_1.png");
+		
+		categories.add(category);
+		
+		
+        category = new Category();
+		
+		//second category
+		category.setId(2);
+		category.setName("Guest House");
+		category.setDescription("This is a description for a guest house");
+		category.setImageURL("CAT_2.png");
+		
+		categories.add(category);
+		
+		
+        category = new Category();
+		
+		//third category
+		category.setId(3);
+		category.setName("Lodge");
+		category.setDescription("This is a description for a lodge");
+		category.setImageURL("CAT_3.png");
+		
+		
+		categories.add(category);
+	}
+	
 	
 	@Override
 	public List<Category> list() {
 		// TODO Auto-generated method stub
-		return null;
+		return categories;
 	}
 
 }
