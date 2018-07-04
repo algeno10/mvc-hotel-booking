a54b1eb6c3afbfa974368ba23421b1c5b9bc5680
George Sikhundla <sikhundla33@gmail.com>
2018-07-04 14:14:08 +0200

    JUnit testing

---
+++ mvcbookingbackend/src/test/java/com/george/bookingbackend/test/CategoryTestCase.java
@@ -0,0 +1,12 @@
+package com.george.bookingbackend.test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.george.mvcbookingbackend.dao.CategoryDAO;
+
+public class CategoryTestCase {
+	
+	private static AnnotationConfigApplicationContext context;
+	
+	private static CategoryDAO categoryDAO;
+
+}
