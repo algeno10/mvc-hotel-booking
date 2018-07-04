9ada792176223d7af18635d93a5318fe58153247
George Sikhundla <sikhundla33@gmail.com>
2018-07-04 16:01:03 +0200

    Updated jUniit

--- mvcbookingbackend/src/main/java/com/george/mvcbookingbackend/daoimpl/CategoryDAOImpl.java
+++ mvcbookingbackend/src/main/java/com/george/mvcbookingbackend/daoimpl/CategoryDAOImpl.java
@@ -1,9 +1,9 @@
 package com.george.mvcbookingbackend.daoimpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,74 +12,76 @@ import com.george.mvcbookingbackend.dao.CategoryDAO;
 import com.george.mvcbookingbackend.dto.Category;
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
     
 	
 	@Autowired
 	private SessionFactory sessionFactory;
 	
-	private static List<Category> categories = new ArrayList<>();
-	
-	static {
-		Category category = new Category();
-		
-		//adding first category
-		category.setId(1);
-		category.setName("Hotel");
-		category.setDescription("This is a description for a hotel");
-		category.setImageURL("CAT_1.png");
-		
-		categories.add(category);
-		
-		
-        category = new Category();
-		
-		//second category
-		category.setId(2);
-		category.setName("Guest House");
-		category.setDescription("This is a description for a guest house");
-		category.setImageURL("CAT_2.png");
-		
-		categories.add(category);
-		
-		
-        category = new Category();
-		
-		//third category
-		category.setId(3);
-		category.setName("Lodge");
-		category.setDescription("This is a description for a lodge");
-		category.setImageURL("CAT_3.png");
-		
-		
-		categories.add(category);
-	}
+
 	
 	
 	@Override
 	public List<Category> list() {
-		// TODO Auto-generated method stub
-		return categories;
+
+        String selectActiveCategory = "FROM Category WHERE active = :active";
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
+        
+		query.setParameter("active", true);
+        return query.getResultList();
 	}
 
-
+    
+	//Getting a single category based on
 	@Override
 	public Category get(int id) {
-		//enhanced for loop
-		for(Category category: categories) {
-			if(category.getId() == id) return category;
-		}
-		return null;
+
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 	}
 
 
 	@Override
-	@Transactional
 	public boolean add(Category category) {
         
 		try {
 			//add the category to the database
 			sessionFactory.getCurrentSession().persist(category);
+			return true;
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+
+	}
+
+    
+	//Method for updating a single category
+	@Override
+	public boolean update(Category category) {
+
+		try {
+			//add the category to the database
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+
+	}
+
+
+	@Override
+	public boolean delete(Category category) {
+        
+		category.setActive(false);
+		
+		try {
+			//add the category to the database
+			sessionFactory.getCurrentSession().update(category);
 			return true;
 			
 		} catch(Exception ex) {
--- mvcbookingbackend/src/test/java/com/george/mvcbookingbackend/test/CategoryTestCase.java
+++ mvcbookingbackend/src/test/java/com/george/mvcbookingbackend/test/CategoryTestCase.java
@@ -27,16 +27,53 @@ public class CategoryTestCase {
 		categoryDAO = (CategoryDAO)context.getBean("categoryDAO");	
 	}
 	
-	@Test
+ /*   @Test
 	public void testAddCategory() {
 		
 		category = new Category();
 		
-		category.setName("Hotel");
-		category.setDescription("This is a description for a hotel");
-		category.setImageURL("CAT_1.png");
+		category.setName("Boutique Hotel");
+		category.setDescription("This is a description for a boutique hotel");
+		category.setImageURL("CAT_4.png");
 		
 		assertEquals("Succesfully added a category inside the table", true, categoryDAO.add(category));
+	} */
+	
+	
+ /* @Test
+	public void testGetCategory() {
+		
+		category = categoryDAO.get(1);
+		assertEquals("Succesfully fetched a category inside the table", "Hotel", category.getName());
+	} */
+	
+	
+/*	@Test
+	public void testUpdateCategory() {
+		
+		category = categoryDAO.get(1);
+		
+		category.setName("Hotel");
+		assertEquals("Succesfully updated a category inside the table", true, categoryDAO.update(category));
+	} */
+	
+	
+/*	@Test
+    public void testDeleteCategory() {
+		
+		category = categoryDAO.get(4);
+		assertEquals("Succesfully deleted a category inside the table", true, categoryDAO.delete(category));
+	} */
+	
+	
+	@Test
+    public void testDeleteList() {
+		
+		assertEquals("Succesfully the list of categories from the table", 3, categoryDAO.list().size());
 	}
+	
+	
+	
+	
 
 }
