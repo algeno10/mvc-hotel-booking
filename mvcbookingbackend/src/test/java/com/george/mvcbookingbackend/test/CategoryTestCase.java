0ec0a4df61fb310a3455f20c9b7932e8a3784887
George Sikhundla <sikhundla33@gmail.com>
2018-07-05 09:03:22 +0200

    Added JUnit tests

--- mvcbookingbackend/src/test/java/com/george/mvcbookingbackend/test/CategoryTestCase.java
+++ mvcbookingbackend/src/test/java/com/george/mvcbookingbackend/test/CategoryTestCase.java
@@ -66,10 +66,48 @@ public class CategoryTestCase {
 	} */
 	
 	
-	@Test
-    public void testDeleteList() {
+/*	@Test
+    public void testListCategory() {
 		
 		assertEquals("Succesfully the list of categories from the table", 3, categoryDAO.list().size());
+	} */
+	
+	@Test
+	public void testCRUDCategory() {
+		
+		//add operation
+        category = new Category();
+		
+		category.setName("Boutique Hotel");
+		category.setDescription("This is a description for a boutique hotel");
+		category.setImageURL("CAT_1.png");
+		
+		assertEquals("Succesfully added a category inside the table", true, categoryDAO.add(category));
+	
+	    
+		
+		category = new Category();
+		
+		category.setName("Bed & Breakfast");
+		category.setDescription("This is a description for a bed & breakfast");
+		category.setImageURL("CAT_2.png");
+		
+		assertEquals("Succesfully added a category inside the table", true, categoryDAO.add(category));
+	
+	    
+		//fetching and updating the category
+        category = categoryDAO.get(2);
+		
+		category.setName("Hotel");
+		
+		assertEquals("Succesfully updated a category inside the table", true, categoryDAO.update(category));
+		
+		
+		//delete the category
+		assertEquals("Succesfully deleted a category inside the table", true, categoryDAO.delete(category));
+	
+	    //fetching the list of categories
+		assertEquals("Succesfully the list of categories from the table", 1, categoryDAO.list().size());
 	}
 	
 	
