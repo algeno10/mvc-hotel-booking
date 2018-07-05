2ac6d0b91c765ae1aeb7549d24c37dfe32c70ab3
George Sikhundla <sikhundla33@gmail.com>
2018-07-05 09:36:10 +0200

    Updated pom.xml and Category database

--- mvcbookingbackend/databaseQueries.sql
+++ mvcbookingbackend/databaseQueries.sql
@@ -8,3 +8,6 @@ CREATE TABLE category(
      
      CONSTRAINT pk_category_id PRIMARY KEY (id)
 );
+
+
+INSERT INTO category(name, description, image_url, is_active) VALUES ('Hotel', 'This is a description for the hotel category', 'CAT_1.png', true);
