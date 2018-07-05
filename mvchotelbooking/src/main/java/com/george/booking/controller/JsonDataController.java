db9cd5a212f655b7129d7f8a2eda9b0cfd6a7252
George Sikhundla <sikhundla33@gmail.com>
2018-07-05 15:48:29 +0200

    Updated Json controllerclass

--- mvchotelbooking/src/main/java/com/george/booking/controller/JsonDataController.java
+++ mvchotelbooking/src/main/java/com/george/booking/controller/JsonDataController.java
@@ -1,5 +1,33 @@
 package com.george.booking.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.george.mvcbookingbackend.dao.PropertyDAO;
+import com.george.mvcbookingbackend.dto.Property;
+
+@Controller
+@RequestMapping("/json/data")
 public class JsonDataController {
+	
+	@Autowired
+	private PropertyDAO propertyDAO;
+	
+	@RequestMapping("/all/property")
+	@ResponseBody
+	public List<Property> getAllProperties(){
+		return propertyDAO.listActiveProperties();
+	}
+	
+	@RequestMapping("/category/{id}/property")
+	@ResponseBody
+	public List<Property> getPropertiesByCategory(@PathVariable int id){
+		return propertyDAO.listActivePropertyByCategory(id);
+	}
 
 }
