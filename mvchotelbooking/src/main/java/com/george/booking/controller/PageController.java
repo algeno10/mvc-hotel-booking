a1805d3ca389b99df1dc5241d9fe1394551d1513
George Sikhundla <sikhundla33@gmail.com>
2018-07-09 10:39:13 +0200

    Updated PageController class

--- mvchotelbooking/src/main/java/com/george/booking/controller/PageController.java
+++ mvchotelbooking/src/main/java/com/george/booking/controller/PageController.java
@@ -1,4 +1,6 @@
 package com.george.booking.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import com.george.mvcbookingbackend.dto.Property;
 
 @Controller
 public class PageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 	
 	@Autowired
 	private CategoryDAO categoryDAO;
